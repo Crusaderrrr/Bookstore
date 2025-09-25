917b3379d5dd42b57e7eb0a3efd6a2b482f0a9f6
Nikita Bulash <bulash2006@gmail.com>
2025-09-25 12:07:40 +0100

    Cloudinary upload/destroy image implemented

--- server/app/src/main/java/com/bookstore/app/controller/CloudinaryController.java
+++ server/app/src/main/java/com/bookstore/app/controller/CloudinaryController.java
@@ -4,7 +4,6 @@ import com.bookstore.app.service.CloudinaryService;
 import java.io.IOException;
 import java.util.Map;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
