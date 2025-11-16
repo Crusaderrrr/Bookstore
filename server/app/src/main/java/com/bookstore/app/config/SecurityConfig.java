55b234148696e7e34bbabd6c3720a8b3b5d87d43
Nikita Bulash <bulash2006@gmail.com>
2025-11-16 14:05:40 +0100

    Tested version of the application that should work

--- server/app/src/main/java/com/bookstore/app/config/SecurityConfig.java
+++ server/app/src/main/java/com/bookstore/app/config/SecurityConfig.java
@@ -81,7 +81,8 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                Arrays.asList("*"));
+                Arrays.asList("http://localhost:3000", "http://localhost:5173",
+                        "http://3.238.252.151:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
